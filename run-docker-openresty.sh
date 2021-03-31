602d1e55740b5092e088dafef29886000ade2791
郑贺公 <zhenghegong@zhenghegongdeMacBook-Pro.local>
2021-03-31 17:21:32 +0900

    add auto run nginx

--- run-docker-openresty.sh
+++ run-docker-openresty.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+BASH_PATH=/Users/zhenghegong/TOOL/docker/docker-openresty
 RUN_CONTAINE_NAME="openresty3"
-USE_IMAGE_NAME="zhghx/centos_openresty:v2"
+USE_IMAGE_NAME="zhghx/centos_openresty:v3"
 
 docker run -d -it -p \
 	6002:80 --privileged=true \
-	-v /Users/zhenghegong/TOOL/openresty/docker/config:/opt/openresty/nginx/conf \
-	-v /Users/zhenghegong/TOOL/openresty/docker/data:/root/data \
+	-v $BASH_PATH/conf:/opt/openresty/nginx/conf \
+	-v $BASH_PATH/data:/root/data \
 	--name $RUN_CONTAINE_NAME $USE_IMAGE_NAME /usr/sbin/init
