c45064cd468d5346aaa53a2e8e416d32fb89c76f
郑贺公 <zhenghegong@zhenghegongdeMacBook-Pro.local>
2021-04-15 23:09:55 +0900

    modifly config

--- run-docker-openresty.sh
+++ run-docker-openresty.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 BASH_PATH=/Users/zhenghegong/TOOL/docker/docker-openresty
-RUN_CONTAINE_NAME="openresty3"
-USE_IMAGE_NAME="zhghx/centos_openresty:v3"
+RUN_CONTAINE_NAME="openresty"
+USE_IMAGE_NAME="openresty/openresty:alpine"
 
 docker run -d -it -p \
 	6002:80 --privileged=true \
-	-v $BASH_PATH/conf:/opt/openresty/nginx/conf \
-	-v $BASH_PATH/data:/root/data \
-	--name $RUN_CONTAINE_NAME $USE_IMAGE_NAME /usr/sbin/init
+	-v $BASH_PATH/conf:/usr/local/openresty/nginx/conf \
+	-v $BASH_PATH/conf/conf.d:/etc/nginx/conf.d \
+	-v $BASH_PATH/log:/usr/local/openresty/nginx/logs \
+	-v $BASH_PATH/html:/usr/local/openresty/nginx/html \
+	--name $RUN_CONTAINE_NAME $USE_IMAGE_NAME
